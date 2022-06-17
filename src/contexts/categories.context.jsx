03620c2803b3de4ecca65cde5661f895a551ee27
Bogna Wierzbinska <bogna8@gmail.com>
2022-06-17 14:04:37 +0200

    create category component and routing to subpages in Shop

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+
+import { addColletionAndDocuments, getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+import SHOP_DATA from "../shop-data";
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    // to tylko raz robimy, inaczej bedzie caly czas dipisywal baze danych
+    // useEffect(() => {
+    //     addColletionAndDocuments('categories', SHOP_DATA);
+    // }, []);
+
+    useEffect(() => {
+        const getCategoriesMap = async () =>{
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        };
+        getCategoriesMap();
+    }, []);
+    const value = {categoriesMap};
+    return (
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+    );
+};
